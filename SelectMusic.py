7a068b76498e30c14428a8ddc528ce3d8e81b384
WitheredLeaf1021 <64011369+WitheredLeaf1021@users.noreply.github.com>
2021-01-02 00:46:20 +0900

    Update SelectMusic.py

--- SelectMusic.py
+++ SelectMusic.py
@@ -6,35 +6,45 @@ argc = len(sys.argv)
 parameters = sys.argv
 
 command = sys.argv[0]
+opFile = sys.argv[1]
 
 count = 0
 LineCount = 0
+fileLine = 0
 random.seed()
 
-if(argc < 2):
+if(argc < 3):
     print("Usage: ", command, "argments .....")
     exit(1)
 
 else:
-    SelectCount = (int)(sys.argv[1])
+    SelectCount = (int)(sys.argv[2])
 
-    if os.path.isfile("MusicList.dat") == False:
-        print("{0}: No such file or directory.".format("MusicList.dat"))
+    if os.path.isfile(opFile) == False:
+        print("{0}: No such file or directory.".format(opFile))
+        exit(1)
+
+    with open (opFile, "r" , encoding="utf-8_sig") as r_file:
+
+        for music in open(opFile , encoding="utf-8_sig").readlines():
+            fileLine += 1
+    
+    if SelectCount > fileLine:
+        print("Input num is over range.")
         exit(1)
 
 index_set = set()
 
 while len(index_set) < SelectCount:
-    index_set.add(random.randint(1 , 95))
+    index_set.add(random.randint(1 , fileLine))
 
 NumberList = list(index_set)
 
 NumberList.sort()
-print(NumberList)
 
-with open ("MusicList.dat", "r" , encoding="utf-8_sig") as r_file:
+with open (opFile, "r" , encoding="utf-8_sig") as r_file:
 
-    for music in open("MusicList.dat" , encoding="utf-8_sig").readlines():
+    for music in open(opFile , encoding="utf-8_sig").readlines():
         LineCount += 1
         name = music.split(",")
 
